e2cbb1bda8d835e23ec14b13d3496622f5b0e2cb
User <User@169.254.63.140>
2022-10-24 11:20:01 -0700

    LessonTwo

--- TestSecond/src/OneTest.java
+++ TestSecond/src/OneTest.java
@@ -4,6 +4,8 @@ public class OneTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
 	}
 	
 }
